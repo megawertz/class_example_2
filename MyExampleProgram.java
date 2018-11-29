4b0182a262662ed351ad84035a9d27d2205dac9a
Jason Wertz <jason.wertz@gmail.com>
2018-11-28 20:25:26 -0500

    Update MyExampleProgram.java
    Creating a conflict

--- MyExampleProgram.java
+++ MyExampleProgram.java
@@ -2,7 +2,7 @@ public class MyExampleProgram {
 
    public static void main(String[] args)
    {
-      System.out.println("REALLY DANGEROUS CODE!");
+      System.out.println("REALLY DANGEROUS CODE! I WILL CREATE A CONFLICT HERE");
       int a = 0/0;
    }
    
