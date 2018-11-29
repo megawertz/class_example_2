5ccf337e8b321ce3d23b4acbafb5365c9da3f221
Jason Wertz <jason.wertz@gmail.com>
2018-11-28 20:27:08 -0500

    New changes.

--- MyExampleProgram.java
+++ MyExampleProgram.java
@@ -2,7 +2,7 @@ public class MyExampleProgram {
 
    public static void main(String[] args)
    {
-      System.out.println("REALLY DANGEROUS CODE!");
+      System.out.println("This is Ok. Not dangerous");
       int a = 0/0;
    }
    
